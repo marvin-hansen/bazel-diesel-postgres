c0e2a208bba3e9cc0bfef9043a44a911a2671ff9
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-12 20:33:50 +0800

    Reproduced error unable to find library -lpq
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,18 @@
+use diesel::{Connection, PgConnection};
 
 fn main() {
+    println!("Hello, Diesel!");
+    let mut connection = postgres_connection();
+    let conn = &mut connection;
 
+    let res = bazel_diesel_postgres::run_db_migration(conn);
+    //dbg!(&result);
+    assert!(res.is_ok());
+}
+
+fn postgres_connection() -> PgConnection {
+    let database_url = "postgres://postgres:postgres@localhost/postgres";
+
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
